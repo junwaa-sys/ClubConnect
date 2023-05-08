ac61a8b2506407694ce89420e7af62774aa23b60
joon jeong <juny.jeong@gmail.com>
2023-05-08 23:34:21 +1200

    change db function for postgres

--- server/db/dashboard.ts
+++ server/db/dashboard.ts
@@ -9,43 +9,41 @@ export async function getMembersSubscriptionById(
   id: string | undefined,
   db = connection
 ): Promise<MemberSubscription> {
-  return (
-    db('member_profiles')
-      .leftJoin(
-        'member_subscriptions',
-        'member_subscriptions.member_id',
-        'member_profiles.stripe_id'
-      )
-      .leftJoin(
-        'subscriptions',
-        'member_subscriptions.subscriptions_id',
-        'subscriptions.id'
-      )
-      .select(
-        'member_subscriptions.member_id as memberId',
-        'member_subscriptions.payment_reference as paymentReference',
-        'member_subscriptions.payment_status as paymentStatus',
-        'member_subscriptions.renewal_date as renewalDate',
-        'member_subscriptions.updated_at as updatedAt',
-        'member_subscriptions.valid_from as validFrom',
-        'member_subscriptions.valid_to as validTo',
-        'member_subscriptions.concession_qty as concessionQty',
-        'subscriptions.name as subscriptionName',
-        'subscriptions.price as subscriptionPrice',
-        'subscriptions.id as subscriptionId',
-        'member_profiles.name as memberName',
-        'member_profiles.auth0_id as auth0id',
-        'member_profiles.address',
-        'member_profiles.emergency_contact_name as emergencyContactName',
-        'member_profiles.emergency_contact_phone as emergencyContactPhone',
-        'member_profiles.phone',
-        'member_profiles.email as memberEmail',
-        'member_profiles.stripe_id as stripeId'
-      )
-      // .where('member_profiles.auth0_id', id)
-      // .orWhere('member_profiles.stripe_id', id)
-      .first()
-  )
+  return db('member_profiles')
+    .leftJoin(
+      'member_subscriptions',
+      'member_subscriptions.member_id',
+      'member_profiles.stripe_id'
+    )
+    .leftJoin(
+      'subscriptions',
+      'member_subscriptions.subscriptions_id',
+      'subscriptions.id'
+    )
+    .select(
+      'member_subscriptions.member_id as memberId',
+      'member_subscriptions.payment_reference as paymentReference',
+      'member_subscriptions.payment_status as paymentStatus',
+      'member_subscriptions.renewal_date as renewalDate',
+      'member_subscriptions.updated_at as updatedAt',
+      'member_subscriptions.valid_from as validFrom',
+      'member_subscriptions.valid_to as validTo',
+      'member_subscriptions.concession_qty as concessionQty',
+      'subscriptions.name as subscriptionName',
+      'subscriptions.price as subscriptionPrice',
+      'subscriptions.id as subscriptionId',
+      'member_profiles.name as memberName',
+      'member_profiles.auth0_id as auth0id',
+      'member_profiles.address',
+      'member_profiles.emergency_contact_name as emergencyContactName',
+      'member_profiles.emergency_contact_phone as emergencyContactPhone',
+      'member_profiles.phone',
+      'member_profiles.email as memberEmail',
+      'member_profiles.stripe_id as stripeId'
+    )
+    .where('member_profiles.auth0_id', id)
+    .orWhere('member_profiles.stripe_id', id)
+  // .first()
 }
 
 export async function createMemberProfile(
